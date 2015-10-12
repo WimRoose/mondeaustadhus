3e4a9108b87eead690a04b148a2044c769ce2c42
Wim Roose <systeembeheer@vooruit.be>
2015-10-13 00:13:14 +0200

    testjes met vlammerd

--- raspi-animation/raspi-cam.sh
+++ raspi-animation/raspi-cam.sh
@@ -4,5 +4,6 @@
 
 # andere opties: raspivid -o - -t 0 -hf -w 640 -h 480 -fps 10 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
 # python?
+# tee? https://en.wikipedia.org/wiki/Tee_(command)
 
 cvlc v4l2:///dev/video0 --v4l2-width 640 --v4l2-height 480 --v4l2-chroma h264 --sout '#standard{access=http,mux=ts,dst=0.0.0.12345}'
